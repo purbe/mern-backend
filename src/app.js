e185e09ada236b3439a3746f0a4cdb3c9b83c901
Nutan <purbeynutan24@gmail.com>
2024-03-07 18:38:42 +0530

    standard syntax

--- src/app.js
+++ src/app.js
@@ -22,8 +22,9 @@ app.use(cookieParser())
 import usersRoute from "./routes/users.routes.js";
 
 //routes declaration
-app.use("/users",usersRoute)
+app.use("api/v1/users",usersRoute)
 
-//https://localhost:8080/users/register
+//http://localhost:8080/users/register
+// http://localhost:8000/api/v1/users/register
 
 export {app};
--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import {app} from "./app.js";
 
 dotenv.config({path:'./env'})
 
-
+//app.get("/",(req,res)=>{"hello world"})
 connectDB()
 .then(
     () => {
-        app.listen(process.env.PORT || 8000,()=>console.log("server is running on port !!"));
+        app.listen(process.env.PORT || 8000,()=>console.log(`server is running on ${process.env.PORT}port !!`));
         //console.log(`server is listening on port 8000`)
     }
 )
